0a38d14ac5cc5b366f2ad928cb2102c8d74e3948
soyamkapasiya <163526543+soyamkapasiya@users.noreply.github.com>
2024-09-24 17:07:27 +0530

    kakeibo-24/09/2024

---
+++ src/main/java/com/kapasiya/account_book_service/AccountBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kapasiya.account_book_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBookServiceApplication.class, args);
+    }
+
+}
